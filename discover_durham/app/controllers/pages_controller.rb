8147d244aa77c51a80b73480bf7fc06973a75569
Jonathan Grieve <mail@jonniegrieve.co.uk>
2026-06-15 16:44:35 +0100

    generate Pages controller

---
+++ discover_durham/app/controllers/pages_controller.rb
@@ -0,0 +1,26 @@
+class PagesController < ApplicationController
+
+    def index
+    end
+
+    def about
+    end
+
+    def attractions
+    end
+
+    def history
+    end
+
+    def events
+    end
+
+    def coffee
+    end
+
+    def links
+    end
+
+    def disclaimer
+    end
+end
